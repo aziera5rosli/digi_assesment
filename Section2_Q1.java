aa882d24f9622bf4f0aea7cf71de29bd35d92004
aziera5rosli <36464931+aziera5rosli@users.noreply.github.com>
2019-12-24 00:26:17 +0800

    Add files via upload

---
+++ Section2_Q1.java
@@ -0,0 +1,15 @@
+
+
+
+public class Section2_Q1 {
+
+public static void main(String[] args) {
+    
+for(int i = 0 ; i < 100; i++) { //Loop from 0 to 100
+      if(i % 3 == 0 || i % 5 == 0 ) //check the number is divisible by 3 or 5
+       System.out.println(i); //print the number if it is divisible by 3 or 5
+
+    }
+}
+}
+
